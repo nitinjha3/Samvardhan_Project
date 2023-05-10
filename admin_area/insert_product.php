1b4b6e5b67462bd60c2b786fe7e84ea6b885c7ce
Nitin <nitinkumarvk100@gmail.com>
2023-05-10 14:29:16 +0530

    10/5

---
+++ admin_area/insert_product.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Insert Products</title>
+        <!-- Bootsstrap Css Link -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+     <!--css file -->
+     <link rel="stylesheet" href="../style.css">
+     <!-- font awesome -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+     <style>
+        .admin_image{
+    width:100px;
+    object-fit:contain; 
+}
+.footer{
+    position: absolute;
+    bottom:0;
+    
+    width:100%;
+}
+</style>
+</head>
+<body class="bg-light">
+    <div class="container my-3">
+        <h1 class="text-center">Insert Products</h1>
+        <!-- form -->
+        <form action="" method="post" enctype="multipart/form-data"></form>
+    </div>
+    
+</body>
+</html>
